8ccf96b32a587862d380d8b68c0d10cbfce69794
clari <clari@host.docker.internal>
2021-11-23 17:06:52 +0100

    Empty Project

---
+++ ScientificProgrammableCalculator/src/main/java/it/unisa/diem/se/group07/scientificprogrammablecalculator/PrimaryController.java
@@ -0,0 +1,17 @@
+package it.unisa.diem.se.group07.scientificprogrammablecalculator;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class PrimaryController {
+
+    @FXML
+    private Button primaryButton;
+
+    @FXML
+    private void switchToSecondary(ActionEvent event) {
+    }
+
+    
+}
---
+++ ScientificProgrammableCalculator/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module it.unisa.diem.se.group07.scientificprogrammablecalculator {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens it.unisa.diem.se.group07.scientificprogrammablecalculator to javafx.fxml;
+    exports it.unisa.diem.se.group07.scientificprogrammablecalculator;
+}
+*/
