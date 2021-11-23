fe4883669539d6f2213fc55c0716ce83b89fed0e
raffa <raffa@LAPTOP-NND9FO0D.mshome.net>
2021-11-23 19:53:55 +0100

    Added getLastTwelve() with test method

--- ScientificProgrammableCalculator/src/test/java/it/unisa/diem/se/group07/scientificprogrammablecalculator/StackComplexNumbersTest.java
+++ ScientificProgrammableCalculator/src/test/java/it/unisa/diem/se/group07/scientificprogrammablecalculator/StackComplexNumbersTest.java
@@ -13,10 +13,11 @@ import org.junit.*;
  * @author DELL
  */
 public class StackComplexNumbersTest {
+
     private StackComplexNumbers stack;
-    
+
     @Before
-    public void setUp(){
+    public void setUp() {
         stack = new StackComplexNumbers();
     }
 
@@ -42,7 +43,46 @@ public class StackComplexNumbersTest {
         ComplexNumbers num = stack.top();
         assertEquals(8.3, num.getReal());
         assertEquals(3.5, num.getImg());
-        
+
     }
-    
+
+    /**
+     * Test of getLastTwelve method, of class StackComplexNumbers.
+     */
+    @Test
+    public void testGetLastTwelve() {
+        int i, dim1=20, dim2 = 5;
+        //test for empty stack
+        ComplexNumbers[] array;
+        array = stack.getLastTwelve();
+        assertEquals(null, array);
+
+        //test for a stack with more than 12 numbers
+        StackComplexNumbers stack1 = new StackComplexNumbers();
+        for (i = 0; i < dim1; i++) {
+            stack1.push(i, i);
+        }
+        ComplexNumbers[] array1;
+        array1 = stack1.getLastTwelve();
+        assertEquals(12, array1.length);
+        for (i = 0; i < 12; i++) {
+            assertEquals(Double.valueOf(dim1-1- i), array1[i].getReal());
+            assertEquals(Double.valueOf(dim1-1-i), array1[i].getImg());
+        }
+
+        //test for a stacks with fewer than 12 numbers
+        StackComplexNumbers stack2 = new StackComplexNumbers();
+        for (i = 0; i < dim2; i++) {
+            stack2.push(i, i);
+        }
+        ComplexNumbers[] array2;
+        array2 = stack2.getLastTwelve();
+        assertEquals(5, array2.length);
+        for (i = 0; i < 5; i++) {
+            assertEquals(Double.valueOf(dim2-1-i), array2[i].getReal());
+            assertEquals(Double.valueOf(dim2-1-i), array2[i].getImg());
+        }
+
+    }
+
 }
