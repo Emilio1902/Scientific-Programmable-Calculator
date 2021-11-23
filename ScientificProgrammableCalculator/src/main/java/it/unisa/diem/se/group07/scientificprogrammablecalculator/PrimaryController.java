df4a16163971db0d8638c76a9f1ff87e91403b31
Emilio1902 <93912962+Emilio1902@users.noreply.github.com>
2021-11-23 16:56:42 +0100

    Delete ScientificProgrammableCalculator directory

--- ScientificProgrammableCalculator/src/main/java/it/unisa/diem/se/group07/scientificprogrammablecalculator/PrimaryController.java
+++
@@ -1,6 +0,0 @@
-package it.unisa.diem.se.group07.scientificprogrammablecalculator;
-
-public class PrimaryController {
-
-    
-}
--- ScientificProgrammableCalculator/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-/* doesn't work with source level 1.8:
-module it.unisa.diem.se.calculator.group01.scientificprogrammablecalculator {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens it.unisa.diem.se.calculator.group01.scientificprogrammablecalculator to javafx.fxml;
-    exports it.unisa.diem.se.calculator.group01.scientificprogrammablecalculator;
-}
-*/
