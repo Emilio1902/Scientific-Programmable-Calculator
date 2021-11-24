73aad5ba83c601fd920cdc0cbdfe72c40b36e77c
clari <clari@host.docker.internal>
2021-11-24 08:38:28 +0100

    Functions pressedKey, deleteNumber and clearDisplay added

--- ScientificProgrammableCalculator/src/main/java/it/unisa/diem/se/group07/scientificprogrammablecalculator/PrimaryController.java
+++ ScientificProgrammableCalculator/src/main/java/it/unisa/diem/se/group07/scientificprogrammablecalculator/PrimaryController.java
@@ -1,6 +1,8 @@
 package it.unisa.diem.se.group07.scientificprogrammablecalculator;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class PrimaryController {
@@ -8,6 +10,27 @@ public class PrimaryController {
     @FXML
     private Label display;
 
+    @FXML
+    private void pressedKey(ActionEvent event) {
+        String number = ((Button) event.getSource()).getText();
+        if (display.getText().contains("Syntax Error")) {
+            this.clearDisplay(event);
+        }
+        display.setText(display.getText() + number);
+    }
 
-    
+    @FXML
+    private void deleteNumber(ActionEvent event) {
+        if (display.getText().length() > 0) {
+            display.setText(display.getText().substring(0, display.getText().length() - 1));
+        }
+    }
+
+    @FXML
+    private void clearDisplay(ActionEvent event) {
+        if (display.getText().length()> 0) {
+            display.setText("");
+        }
+    }
+
 }
