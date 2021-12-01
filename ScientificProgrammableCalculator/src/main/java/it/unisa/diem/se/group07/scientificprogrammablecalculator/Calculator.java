36115340dd91c849943764946bae1012a60bc667
Diego Fileno <Diego Fileno@LAPTOP-LUC1FHPA.mshome.net>
2021-12-01 12:31:26 +0100

    To integrate the product code with the functionalities of the graphic interface (<x and >x)

--- ScientificProgrammableCalculator/src/main/java/it/unisa/diem/se/group07/scientificprogrammablecalculator/Calculator.java
+++ ScientificProgrammableCalculator/src/main/java/it/unisa/diem/se/group07/scientificprogrammablecalculator/Calculator.java
@@ -5,8 +5,6 @@
  */
 package it.unisa.diem.se.group07.scientificprogrammablecalculator;
 
-import java.math.BigDecimal;
-
 /**
  * Calculator implements a calculator
  *
@@ -15,17 +13,19 @@ import java.math.BigDecimal;
 public class Calculator {
 
     private StackComplexNumbers memory;
+    private Variables variables;
 
     /**
      * Constructs the stack memory of the calculator
      */
     public Calculator() {
         memory = new StackComplexNumbers();
+        variables = new Variables();
     }
 
     /**
-     * Constructs the string to pass to the calculator and checks 
-     * math operations.
+     * Constructs the string to pass to the calculator and checks math
+     * operations.
      *
      * @param s String to pass to the calculator
      * @return "" if the string format is correct otherwise "Syntax Error" if
@@ -35,38 +35,35 @@ public class Calculator {
 
         if (s.compareTo("") == 0) {
             return "";
-        } 
-        
+        }
+
         if (s.compareTo("+") == 0) {
             return memory.sumLastTwoNumbers() == true ? "" : "Math Error";
-        } 
-        
+        }
+
         if (s.compareTo("-") == 0) {
             return memory.differenceLastTwoNumbers() == true ? "" : "Math Error";
-        } 
-        
+        }
+
         if (s.compareTo("*") == 0) {
             return memory.productLastTwoNumbers() == true ? "" : "Math Error";
-        } 
-        
+        }
+
         if (s.compareTo("/") == 0) {
             return memory.ratioLastTwoNumbers() == true ? "" : "Math Error";
         }
-        
+
         if (s.compareTo("√") == 0) {
             return memory.squareRootLastNumber() == true ? "" : "Math Error";
         }
-        
+
         if (s.compareTo("+-") == 0) {
             return memory.invertSignLastNumber() == true ? "" : "Math Error";
-        }
-     
-        
-        else {
+        } else {
 
             ComplexNumbers num = new ComplexNumbers(s);
-            
-            if (num.getReal()!=Double.NEGATIVE_INFINITY) {
+
+            if (num.getReal() != Double.NEGATIVE_INFINITY) {
                 memory.push(num.getReal(), num.getImg());
                 return "";
             } else {
@@ -75,45 +72,88 @@ public class Calculator {
 
         }
     }
-    
+
     /**
-     * Constructs the string to pass to the calculator and checks 
-     * memory operations.
+     * Constructs the string to pass to the calculator and checks memory
+     * operations.
      *
      * @param s String to pass to the calculator
      * @return "" if the string format is correct otherwise "Syntax Error" if
      * the string format is incorrect
      */
-     public String checkMemoryOperations(String s) {
- 
-        
+    public String checkMemoryOperations(String s) {
+
         if (s.compareTo("clear") == 0) {
             memory.clear();
             return "";
-        } 
-        
+        }
+
         if (s.compareTo("drop") == 0) {
             return memory.dropLastNumber() == true ? "" : "Few Arguments";
         }
-         
+
         if (s.compareTo("dup") == 0) {
             return memory.dupLastNumber() == true ? "" : "Few Arguments";
         }
-          
+
         if (s.compareTo("swap") == 0) {
             return memory.swapLastTwoNumbers() == true ? "" : "Few Arguments";
         }
-           
+
         if (s.compareTo("over") == 0) {
             return memory.dupSecondLastNumber() == true ? "" : "Few Arguments";
+        } else {
+            return "Syntax Error";
         }
-        
-        else {
-                return "Syntax Error";
-            }
 
     }
 
+    /**
+     * Constructs the string to pass to the calculator and checks memory
+     * operations.
+     *
+     * @param op
+     * @param var
+     * @return "" if the string format is correct otherwise "Syntax Error" if
+     * the string format is incorrect
+     */
+    public String checkVariableOperations(String operation, String variable) {
+
+        ComplexNumbers num;
+        char op, var;
+        
+        if (operation.length() == 1 && variable.length() == 1) {
+            op = operation.charAt(0);
+            var = variable.charAt(0);
+        } else 
+            return "Syntax Error";
+        
+        if (op == '>') {
+            num = memory.pop();
+            if (num == null) {
+                return "Few Arguments";
+            } else {
+                return variables.setVariableValue(var, num) == true ? "" : "Variable Error";
+            }
+        }
+
+        if (op == '<') {
+            num = variables.getVariableValue(var);
+
+            if (num == null) {
+                return "No Value Stored";
+            } else {
+                memory.push(num.getReal(), num.getImg());
+                return "";
+            }
+
+        } else {
+            return "Syntax Error";
+        }
+
+    }
+
+    /*
     /**
      * Returns the last twelve numbers in the stack memory.
      * 
--- ScientificProgrammableCalculator/src/main/java/it/unisa/diem/se/group07/scientificprogrammablecalculator/PrimaryController.java
+++ ScientificProgrammableCalculator/src/main/java/it/unisa/diem/se/group07/scientificprogrammablecalculator/PrimaryController.java
@@ -106,6 +106,12 @@ public class PrimaryController implements Initializable{
     }
 
     @FXML
-    private void makeVariableOperations(ActionEvent event) {
+    private void makeVariableOperations(ActionEvent event) { 
+        String varOp = variableOperations.getValue();
+        String var = variables.getValue();
+        String s1 = calculator.checkVariableOperations(varOp, var);
+        display.setText(s1);
+        numbersList.getItems().clear();
+        numbersList.getItems().addAll(calculator.lastTwelveNumbers());
     }
 }  
