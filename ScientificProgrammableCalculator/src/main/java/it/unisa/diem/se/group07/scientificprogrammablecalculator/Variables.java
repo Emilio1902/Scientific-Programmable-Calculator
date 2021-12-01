9322ce902d290fe3594b384ff24a2009d114205b
raffa <raffa@LAPTOP-NND9FO0D.mshome.net>
2021-12-01 10:47:49 +0100

    Added Variables class

---
+++ ScientificProgrammableCalculator/src/main/java/it/unisa/diem/se/group07/scientificprogrammablecalculator/Variables.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.diem.se.group07.scientificprogrammablecalculator;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author raffa
+ */
+public class Variables {
+    private HashMap<String, String> variables;
+
+    public Variables() {
+        this.variables = new HashMap<String, String>();
+    }
+    
+    
+    
+}
